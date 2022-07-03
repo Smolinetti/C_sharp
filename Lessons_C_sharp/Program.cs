716402548f7c5a3607346342ad1345e6068c407f
Smolinetti <2407373@mail.ru>
2022-07-03 17:11:48 +0300

    Task_15

--- Lessons_C_sharp/Program.cs
+++ Lessons_C_sharp/Program.cs
@@ -22,4 +22,13 @@ if (n > 99)
 }
 
 
+Console.WriteLine("Введите день недели от 1 до 7");
+string numberDay = Console.ReadLine();
+if (numberDay == "6" || numberDay == "7")
+{
+    Console.WriteLine("Это выходной день !");
+} else {
+    Console.WriteLine("Это будний день !");
+}
+
 
