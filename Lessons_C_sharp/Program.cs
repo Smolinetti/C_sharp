816ebd3ca9801f3a263cca6a397f96cdde82b3ed
Smolinetti <2407373@mail.ru>
2022-07-23 17:26:28 +0300

    Task_54

--- Lessons_C_sharp/Program.cs
+++ Lessons_C_sharp/Program.cs
@@ -1,155 +1,67 @@
-﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
-Console.WriteLine($"Задача 47: Задайте двумерный массив размером m x n, заполненный случайными вещественными числами.");
-
-Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
 Console.Clear();
-Console.WriteLine($"m = {m}, n = {n}.");
+Console.WriteLine($"Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
 
-double[,] array = new double[m, n];
-
-CreateArrayDouble(array);
-
+int[,] array = new int[m, n];
+CreateArray(array);
 WriteArray(array);
 
-Console.WriteLine();
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
 
-void CreateArrayDouble(double[,] array)
+void OrderArrayLines(int[,] array)
 {
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            array[i, j] = new Random().NextDouble() * 20 - 10;
-        }
-    }
-}
-
-void WriteArray(double[,] array)
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            double alignNumber = Math.Round(array[i, j], 1);
-            Console.Write(alignNumber + " ");
-        }
-        Console.WriteLine();
-    }
-}
-
-
-// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-Console.WriteLine($"Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
-
-Console.Write("\nMассив возьмем из предыдущей задачи (№ 47).\n");
-Console.Write("Введите координаты позиции элемента, разделенных запятой: ");
-
-string? positionElement = Console.ReadLine();
-positionElement = RemovingSpaces(positionElement);
-int[] position = ParserString(positionElement);
-
-if (position[0] <= m
-&& position[1] <= n
-&& position[0] >= 0
-&& position[1] >= 0)
-{
-    double result = array[position[0] - 1, position[1] - 1];
-    Console.Write($"Значение элемента: {result}");
-}
-else Console.Write($"такого элемента в массиве нет.");
-
-int[] ParserString(string input)
-{
-    int countNumbers = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-            countNumbers++;
-    }
-
-    int[] numbers = new int[countNumbers];
-
-    int numberIndex = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        string subString = String.Empty;
-
-        while (input[i] != ',')
-        {
-            subString += input[i].ToString();
-            if (i >= input.Length - 1)
-                break;
-            i++;
-        }
-        numbers[numberIndex] = Convert.ToInt32(subString);
-        numberIndex++;
-    }
-
-    return numbers;
-}
-
-string RemovingSpaces(string input)
-{
-    string output = String.Empty;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] != ' ')
-        {
-            output += input[i];
-        }
-    }
-    return output;
-}
-
-// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-Console.WriteLine($"\n\nЗадача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
-Console.Write("\nMассив возьмем из предыдущей задачи (№ 47), преобразовав double в int32.\n");
-
-int[,] arrayWhole = new int[m, n];
-arrayWhole = TransformationArrayWhole(array);
-
-WriteArrayInt(arrayWhole);
-
-Console.Write($"\nCреднее арифметическое:\n");
-for (int i = 0; i < n; i++)
-{
-    double arithmeticMean = 0;
-    for (int j = 0; j < m; j++)
-    {
-        arithmeticMean += arrayWhole[j, i];
-    }
-    arithmeticMean = Math.Round(arithmeticMean / m, 1);
-    Console.WriteLine($"столбца № {i + 1} {arithmeticMean}");
-}
-
-int[,] TransformationArrayWhole(double[,] array)
-{
-    int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
         }
     }
-    return arrayWhole;
 }
 
-void WriteArrayInt(int[,] arrayWhole)
+int InputNumbers(string input)
 {
-    for (int i = 0; i < m; i++)
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(arrayWhole[i, j] + " ");
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
+
+
